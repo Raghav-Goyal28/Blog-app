f115ef413f336327af0032b19573524539cabc99
Raghav-Goyal28 <rg773265@gmail.com>
2024-09-18 18:00:48 +0530

    Completed

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,18 +1,18 @@
 
 
 
-
+import { useAuth } from "../context/AuthProvider";
 
 function About() {
-
-;
+  const { profile } = useAuth();
+  console.log(profile);
   return (
     <div className="container mx-auto my-12 p-4 space-y-9">
       <h1 className="text-2xl font-bold mb-6">About</h1>
       <p>
         This is{" "}
         <strong className="text-blue-800 font-semibold hover:scale-105 duration-500">
-         Raghav Goyal
+          {profile?.user?.name}
         </strong>{" "}
         a proficient full stack developer with a robust skill set spanning both
         front-end and back-end technologies. With a passion for building
@@ -48,24 +48,14 @@ function About() {
       </p>
       <br />
       <span>
-        Akhil K is dedicated to leveraging his expertise to contribute to
+      {profile?.user?.name} is dedicated to leveraging his expertise to contribute to
         innovative projects and drive technological advancements. Whether
         working on front-end interfaces or back-end logic, he is passionate
         about delivering exceptional digital solutions that meet user needs and
         exceed client expectations.
       </span>
-      <h2 className="font-semibold text-blue-800 text-xl">
-        Personal Interests and Inspiration:
-      </h2>
-      <p>
-        Beyond his professional achievements, Akhil is a big fan of cricket and
-        holds immense admiration for <strong> King Kohli.</strong> His favorite
-        person and biggest inspiration is his twin brother,{" "}
-        <strong>Ankush.</strong> Their friendly rivalry and deep bond have
-        significantly shaped Akhil’s journey. Ankush is not only a great
-        competitor but also a steadfast friend, constantly motivating Akhil to
-        strive for excellence.
-      </p>
+     
+      
     </div>
   );
 }
