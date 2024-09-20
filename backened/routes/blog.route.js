c15d1655af8a84ee8a21bd20ff79255c0a80f5f1
Raghav-Goyal28 <rg773265@gmail.com>
2024-09-20 23:43:54 +0530

    tr

--- backened/routes/blog.route.js
+++ backened/routes/blog.route.js
@@ -6,7 +6,7 @@ const router=express.Router();
 router.post("/create",isAuthenticated,isAdmin("admin"),createBlog);
 router.delete("/delete/:id",isAuthenticated,isAdmin("admin"),deleteBlog)
 router.get("/all-blogs",getAllBlogs)
-router.get("/single-blog/:id",isAuthenticated,getSingleBlogs)
+router.get("/single-blog/:id",getSingleBlogs)
 router.get("/my-blog",isAuthenticated,isAdmin("admin"),getMyBlogs);
 router.put("/update/:id", isAuthenticated, isAdmin("admin"), updateBlog);
 export default router;
